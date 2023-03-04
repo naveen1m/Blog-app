51c9059d1500d3e2c97c088ad54eeab08e39594d
naveen <stupiddint@gmail.com>
2023-03-04 21:29:33 +0530

    all route added

--- server/router/route.js
+++ server/router/route.js
@@ -5,11 +5,11 @@ const router = Router();
 
 /** POST : POST requests are used to send data to the API server and crearte or update a resources. */
 router.route('/register').post(controller.register);
-router.route('/login').post();
-router.route('/getblog').get();
-router.route('/postblog').post();
-router.route('/editblog').put();
-router.route('/deleteblog').delete();
+router.route('/login').post(controller.login);
+router.route('/getblog').get(controller.getblog);
+router.route('/postblog').post(controller.postblog);
+router.route('/editblog').put(controller.editblog);
+router.route('/deleteblog').delete(controller.deleteblog);
 
 
 export default router;
