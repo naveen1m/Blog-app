b34c7ad85ef0047b293ed3651778cc36b2601148
naveen <stupiddint@gmail.com>
2023-03-04 21:23:56 +0530

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
 import React from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import PostPage from "./pages/PostPage";
 
 function App() {
   return (
-    <h1 className="text-green-500">Blog page creating...</h1>
-  )
+    <Routes>
+      <Route
+        index
+        element={
+          <main className="bg-gray-100  ">
+            <Header />
+            <PostPage />
+            <Footer />
+          </main>
+        }
+      />
+      <Route exact path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+    </Routes>
+  );
 }
 export default App;
--- server/server.js
+++ server/server.js
@@ -1,16 +1,33 @@
 import express from "express";
 import cors from 'cors';
+import connect from "./database/mongodbConnection.js";
+import router from "./router/route.js";
 
 
 const port = process.env.PORT || '8080'
 /** middlewares */
 const app = express();
 app.use(express.json());
+app.use(cors());
+app.disable('x-powered-by');  // less hacker know about our stack
 
+/** HTTP request */
 app.get('/',(req,res)=>{
     res.json('hello bro, its blog backend.')
 })
 
-app.listen(port, ()=>{
-    console.log(`server running at http://localhost:${port}`)
-})
+/** api routes */
+app.use('/api',router)
+
+/** start server only when we have valid db connection */
+connect().then(()=>{
+    try {
+        app.listen(port, ()=>{
+            console.log(`server running at http://localhost:${port}`)
+        })
+    } catch (error) {
+        console.log(`cannot connect to server ${error}`)
+    }
+
+}).catch(error => console.log(`invalid db connection`))
+
