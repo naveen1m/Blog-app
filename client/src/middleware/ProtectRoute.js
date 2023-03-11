12481c8d144e9cca3e2d4f057ab0ad3d270143b3
naveen <stupiddint@gmail.com>
2023-03-11 12:28:33 +0530

    protect route

---
+++ client/src/middleware/ProtectRoute.js
@@ -0,0 +1,19 @@
+import { Navigate } from "react-router-dom";
+
+export const AuthorizeUser = ({children})=>{
+    const token = localStorage.getItem('token');
+    if(token){
+        return <Navigate to={'/'} replace={true} ></Navigate>
+    }
+    return children;
+}
+
+const ProtectRoute = ({children})=>{
+    const token = localStorage.getItem('token');
+    if(!token){
+        return <Navigate to={'/'} replace={true} ></Navigate>
+    }
+    return children;
+}
+
+export default ProtectRoute
